e024c344ada10931a0914b66bb2d9cf43b1e08a8
James Msechu <jamesmsechu@Jamess-MBP.lan>
2024-08-15 16:18:30 -0700

    fixing sign out redirect uri

--- app/ui/signout/SignoutBar.tsx
+++ app/ui/signout/SignoutBar.tsx
@@ -11,7 +11,7 @@ export default function SignoutBar() {
   
     <Link
         className=""
-        href="http://localhost:3000"
+        href="https://spottydog.vercel.app"
       >
         <button type="button" className= "hover:bg-slate-700 border-2 border-slate-900 bg-slate-800 w-9/12 rounded-full mb-4">Sign-Out</button>
       </Link>
